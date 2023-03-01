045b8b79da0361532339158d523781b5bb8e56dd
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-03-01 18:57:30 +0100

    feat: :bento: Add images for what we do section

--- app/components/WhatWeDo.tsx
+++ app/components/WhatWeDo.tsx
@@ -10,33 +10,52 @@ export default function WhatWeDo() {
           to="/catalog"
           className="flex flex-col gap-4 rounded-2xl bg-white text-center transition-colors duration-200 hover:bg-gradient-to-b hover:from-red-200 hover:to-red-600 hover:text-white"
         >
-          <p>Imagen</p>
+          <img
+            src="https://res.cloudinary.com/dfp8qzzku/image/upload/v1677692569/images/shopping-basket-logo_nepv2p.png"
+            alt="Una cesta de la compra"
+            loading="lazy"
+            className="mx-auto mt-4 w-2/5"
+          />
           <h3 className="font-bold">Nuestro catálogo</h3>
           <p className="p-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sint
-            nostrum ab.
+            Descubre nuestro catálogo de productos para mascotas: desde comida
+            hasta juguetes, tenemos todo lo que necesitas para mantener feliz a
+            tu compañero peludo. ¡Haz clic aquí para empezar a explorar!
           </p>
         </Link>
         <Link
           to="/adopt"
           className="flex flex-col gap-4 rounded-2xl bg-white text-center transition-colors duration-200 hover:bg-gradient-to-b hover:from-blue-700 hover:via-blue-800 hover:to-gray-900 hover:text-white"
         >
-          <p>Imagen</p>
+          <img
+            src="https://res.cloudinary.com/dfp8qzzku/image/upload/v1677691991/images/adoptable-logo_hs2vfq.png"
+            alt="Una patita de un animal"
+            loading="lazy"
+            className="mx-auto mt-4 w-2/5"
+          />
           <h3 className="font-bold">Peludos en adopción</h3>
           <p className="p-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sint
-            nostrum ab.
+            ¿Buscas darle un hogar amoroso a una mascota necesitada? ¡Mira
+            nuestra sección de mascotas adoptables! Tenemos una variedad de
+            perros y gatos que están buscando su hogar para siempre.
           </p>
         </Link>
         <Link
           to="/tips"
           className="flex flex-col gap-4 rounded-2xl bg-white text-center transition-colors duration-200 hover:bg-gradient-to-b hover:from-purple-200 hover:via-purple-400 hover:to-purple-800 hover:text-white"
         >
-          <p>Imagen</p>
+          <img
+            src="https://res.cloudinary.com/dfp8qzzku/image/upload/v1677692839/images/tips-care-logo_zoysnb.png"
+            alt="Un humano y un gato abrazados"
+            loading="lazy"
+            className="mx-auto mt-4 w-2/5"
+          />
           <h3 className="font-bold">Consejos y cuidados</h3>
           <p className="p-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sint
-            nostrum ab.
+            ¿Quieres asegurarte de que tu mascota esté sana y feliz? Echa un
+            vistazo a nuestra sección de consejos y cuidados de mascotas, donde
+            encontrarás información útil sobre nutrición, ejercicios, cuidado
+            dental y más.
           </p>
         </Link>
       </div>
