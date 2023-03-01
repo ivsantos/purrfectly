48e7bf9f278e587af671ef257686217a1ad22b61
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-03-01 23:18:26 +0100

    fix: :bug: Fix duplicated keys for rating component

--- app/components/Rating.tsx
+++ app/components/Rating.tsx
@@ -10,9 +10,9 @@ export default function Rating({ rating }: RatingProps) {
       <h3 className="sr-only">Reseñas</h3>
       <div className="flex items-center">
         <div className="flex items-center">
-          {[0, 1, 2, 3, 4].map((num) => (
+          {[0, 1, 2, 3, 4].map((num, idx) => (
             <StarIcon
-              key={rating}
+              key={`${rating}-${idx}`}
               className={`${
                 rating > num ? 'text-highlight' : 'text-gray-300'
               } h-5 w-5 flex-shrink-0`}
